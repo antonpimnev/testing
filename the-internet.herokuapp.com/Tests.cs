b2f0e8eb55a3e5fc99ef99257992a3695592d02d
Anton Pimnev <anton.pimnev@gmail.com>
2022-09-29 20:09:16 +0500

    1Init

---
+++ the-internet.herokuapp.com/Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace the_internet.herokuapp.com
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
